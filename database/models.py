0cd2e6bd90ebbd937f41ad2b12d3d98d7a61e755
ArturStepanyan10 <stepanyanartur110@gmail.com>
2026-03-15 21:41:06 +0300

    убрал миксин, прописав поля в базовом классе

--- database/models.py
+++ database/models.py
@@ -1,38 +1,57 @@
 from datetime import datetime
 
-from sqlalchemy import String, Integer, DateTime, Text, SmallInteger, func, ForeignKey, CheckConstraint
+from sqlalchemy import (
+    CheckConstraint,
+    DateTime,
+    ForeignKey,
+    Integer,
+    SmallInteger,
+    String,
+    Text,
+    func,
+)
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-
-from database.mixins.TimeStampMixin import TimestampMixin
 
 
 class Base(DeclarativeBase):
-    pass
-
-
-class Category(Base, TimestampMixin):
-    __tablename__ = 'category'
-
     id: Mapped[int] = mapped_column(primary_key=True)
+    created_at: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True), default=func.now(), nullable=False
+    )
+    updated_at: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True),
+        default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
+
+
+class Category(Base):
+    __tablename__ = "category"
+
     title: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
-    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'), ondelete='CASCADE', nullable=False)
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("user.id"), ondelete="CASCADE", nullable=False
+    )
 
 
-class Item(Base, TimestampMixin):
-    __tablename__ = 'item'
+class Item(Base):
+    __tablename__ = "item"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(255), nullable=False)
     review: Mapped[str] = mapped_column(Text)
-    rating: Mapped[int] = mapped_column(SmallInteger, CheckConstraint("rating BETWEEN 1 AND 5"))
+    rating: Mapped[int] = mapped_column(
+        SmallInteger, CheckConstraint("rating BETWEEN 1 AND 5")
+    )
     image_url: Mapped[str] = mapped_column(Text)
-    category_id: Mapped[int] = mapped_column(ForeignKey("category.id"), ondelete='CASCADE', nullable=False)
+    category_id: Mapped[int] = mapped_column(
+        ForeignKey("category.id"), ondelete="CASCADE", nullable=False
+    )
 
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = "user"
 
-    id: Mapped[int] = mapped_column(primary_key=True,)
     name: Mapped[str] = mapped_column(String(255), nullable=False)
     last_name: Mapped[str] = mapped_column(String(255), nullable=False)
     middle_name: Mapped[str | None] = mapped_column(String(255))
