35c86da61ec099dfbf4a822b09defbfd4b17c312
ArturStepanyan10 <stepanyanartur110@gmail.com>
2026-05-15 22:59:30 +0300

    изменил состояние формы категории

--- states/form_category_state.py
+++ states/form_category_state.py
@@ -1,10 +1,12 @@
 from aiogram.fsm.state import State, StatesGroup
 
-
+ 
 class FSMFillFormCategoryState(StatesGroup):
     """
     Класс для состояния бота. Тут перечислены состояния ожидания
     и в данных состояних будет находится БОТ
     """
 
-    fill_title = State()  # Состояние ожидания ввода названия категории
+    fill_category_id = State()  # Состояние ожидания выбора категории для изменения или удаления
+    fill_add_title = State()  # Состояние ожидания ввода названия категории
+    fill_update_title = State()  # Состояние ожидания ввода нового названия категории
