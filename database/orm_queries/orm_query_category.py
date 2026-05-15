273cf39cd640c9e7f79c5396900f3fd89ce1ab1d
ArturStepanyan10 <stepanyanartur110@gmail.com>
2026-05-15 23:16:15 +0300

    прописал хендлеры для работы с категориями и подправил орм запросы для таблицы категория

--- database/orm_queries/orm_query_category.py
+++ database/orm_queries/orm_query_category.py
@@ -1,6 +1,8 @@
-from database.models import Category
+from sqlalchemy import delete, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select, update, delete
+
+from database.models import Category
+
 
 async def orm_get_all_categories(session: AsyncSession):
     query = select(Category)
@@ -9,32 +11,27 @@ async def orm_get_all_categories(session: AsyncSession):
 
 
 async def orm_add_categories(session: AsyncSession, data: dict):
-    obj = Category(
-        title=data.get("title"),
-        user_id=data.get("user_id")
-    )
+    obj = Category(title=data.get("title"), user_id=data.get("user_id"))
+    
+    # Сохраняем категорию в БД (commit происходит в middleware)
     session.add(obj)
-    await session.commit()
-    
-    
-async def orm_get_category(session: AsyncSession, category_id: int):
+
+
+async def orm_get_category_by_id(session: AsyncSession, category_id: int):
     query = select(Category).where(Category.id == category_id)
     result = await session.execute(query)
-    return result.scalars().all()
+    return result.scalar_one_or_none()
 
 
 async def orm_update_category(session: AsyncSession, category_id: int, data: dict):
     query = (
         update(Category)
         .where(Category.id == category_id)
-        .values(
-            title=data.get("title"),
-            user_id=data.get("user_id")
-        )
+        .values(title=data.get("title"), user_id=data.get("user_id"))
     )
     await session.execute(query)
     await session.commit()
-    
+
 
 async def orm_delete_category(session: AsyncSession, category_id: int):
     query = delete(Category).where(Category.id == category_id)
